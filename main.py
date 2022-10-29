3c1f0ebf41458ed12ccf432eca2320874be5a0d7
andy <a@a.a>
2022-10-29 17:10:20 -0500

    actual embed progress

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import discord
 # Creates a 'Client.' Clients are the connection between the code and discord
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
 # Registers an event
@@ -20,13 +19,21 @@ async def on_message(message): # On Message happens everytime a message is sent
     if message.content.startswith('$hello'): #if the message starts w "$hello" the bot will reply "Hello!"
         await message.channel.send('blake its me ur father, i want to tell you something (reply "$what" to find out what your father wants to tell you')
 
-    if message.content.startswith('$what'):  # if the message starts w "$hello" the bot will reply "Hello!"
+    if message.content.startswith('$what'):
         await message.channel.send('ur gay')
 
-@client.command()
-async def embed(ctx):
-    embed = discord.Embed(title='Yo Mama', url='https://youtu.be/S9uTScSgzrM', description='pussy balls and ass', color='#89CFF0')
-    await ctx.send(embed.embed)
+    embedT = discord.Embed(
+        title='GUYS CHECK THIS OUT',
+        description='this is the coolest SHIT ive ever seen',
+        url='https://youtu.be/S9uTScSgzrM',
+        color=discord.Color.teal(),
+
+
+    )
+
+    if message.content.startswith('$embed'):
+        await message.channel.send(embed=embedT)
+
 
 with open("token", "r") as fd:
     client.run(fd.read())
