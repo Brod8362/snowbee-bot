d2c1369c6a1c8c32fa49b193941aac78e0bf7321
andy <a@a.a>
2022-10-29 21:42:47 -0500

    Cleaning File and created Embed Build function

--- main.py
+++ main.py
@@ -10,46 +10,20 @@ r = requests.get('http://api.snowbee.byakuren.pw/info')
 json = r.json()
 json["version"]
 
-# Registers an event
+def build_embed(tosearch):
+    embedObj = discord.Embed(
+        title=tosearch,
+        color=discord.Color.teal()
+    )
+    embedObj.add_field(name='Price:', value='One Billion Dollars', inline=False)
+    embedObj.add_field(name='Seller:', value='Blake Rodriguez', inline=False)
+    embedObj.add_field(name='Rating:', value='11/10', inline=False)
+
+    return embedObj
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-@client.event
-async def on_message(message): # On Message happens everytime a message is sent
-    if message.author == client.user: # This returns if the message is sent by the bot
-        return
-
-    if message.content.startswith('$hello'): #if the message starts w "$hello" the bot will reply "Hello!"
-        await message.channel.send('blake its me ur father, i want to tell you something (reply "$what" to find out what your father wants to tell you')
-
-    if message.content.startswith('$what'):
-        await message.channel.send(json["version"])
-
-
-    embedT = discord.Embed(
-        title='Vibrator',
-        url='https://a.co/d/j5qZpNj',
-        color=discord.Color.teal()
-    )
-    embedT.set_thumbnail(url='https://m.media-amazon.com/images/G/01/apparel/rcxgs/tile._CB483369110_.gif')
-    embedT.add_field(name="Price:",
-                     value='$35.99',
-                     inline=False)
-    embedT.add_field(name='Seller:',
-                     value='Amazon',
-                     inline=False)
-    embedT.add_field(name='Color:',
-                     value='Purple',
-                     inline=False)
-    embedT.add_field(name='Rating:',
-                     value='4.3 / 5',
-                     inline=False)
-    embedT.add_field(name='Shipping Time',
-                     value='Overnight',
-                     inline=False)
-
-    if message.content.startswith('$embed'):
-        await message.channel.send(embed=embedT)
 
 @client.event
 async def on_message(message):  # Reads every message sent
@@ -60,6 +34,7 @@ async def on_message(message):  # Reads every message sent
     if message.content.startswith('$search'):
         tosearch = message.content[8:]
         await message.channel.send('Searching for "' + tosearch + '"')
+        await message.channel.send(embed=build_embed(tosearch))
 
 
 
